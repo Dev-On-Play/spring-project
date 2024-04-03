49eb4983f5a1bc6b423e963481c84ccb2ca5cb1c
rave189 <rave189@naver.com>
2024-04-04 00:05:22 +0900

    [FE] feat: 프로필 영역 구현

--- frontend/app/mypage/page.tsx
+++ frontend/app/mypage/page.tsx
@@ -1,5 +1,26 @@
-"use client"
+"use client";
+
+import CommonAvatar from "@/components/common/Avatar";
+import CommonDialog from "@/components/common/Dialog";
+import { Label } from "@/components/ui/label";
+import { Switch } from "@/components/ui/switch";
 
 export default function MyPage() {
-  return <>마이페이지</>
+  return (
+    <div className="flex space-x-4">
+      <CommonAvatar className="size-20" imgSrc={"https://github.com/shadcn.png"} />
+      <div className="space-y-4">
+        <div className="flex space-x-6">
+          <CommonDialog className="" triggerText="Nickname" dialogTitle="Edit Nickname" dialogLabelText="Name"></CommonDialog>
+          <div className="flex flex-1 self-center space-x-2">
+            <Switch />
+            <div className="self-start">
+              <Label>온라인</Label>
+            </div>
+          </div>
+        </div>
+        <CommonDialog className="" triggerText="Introduce Myself" dialogTitle="Edit Introduce" dialogLabelText="Introduce"></CommonDialog>
+      </div>
+    </div>
+  )
 }
--- frontend/components/common/Avatar.tsx
+++ frontend/components/common/Avatar.tsx
@@ -7,15 +7,17 @@ interface Props {
   imgSrc: string
   alt?: string
   fallback?: string
+  className: string
 }
 
 const CommonAvatar: NextPage<Props> = ({
   imgSrc,
   alt = "@shadcn",
   fallback = "CN",
+  className
 }) => {
   return (
-    <Avatar>
+    <Avatar className={className}>
       <AvatarImage src={imgSrc} alt={alt} />
       <AvatarFallback>{fallback}</AvatarFallback>
     </Avatar>
