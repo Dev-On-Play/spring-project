d43edbf4ab7572136e09e30dba51a765f554234b
권준혁 / Kwon Joonhyeok <45958283+jundoopop@users.noreply.github.com>
2024-05-01 17:08:57 +0900

    [FE] feat:  카드 뷰 컴포넌트 생성
    # 방식
    
    - json 파일에 있는 데이터를 mainPage/page.tsx 에서 불러와서 각 컴포넌트에 json 데이터를 표시함
    
    # mogakoCard.tsx
    
    - 카드뷰 재사용성을 높이기 위한 리액트 컴포넌트
    - shadcn 활용했음
    
    # cardViewExData.json
    
    - db 데이터 생성 전이라 임의로 카드뷰 테스트를 위한 무작위 값을 넣은 데이터 파일

--- frontend/app/mainPage/page.tsx
+++ frontend/app/mainPage/page.tsx
@@ -1,5 +1,67 @@
-"use client";
+"use client"
+
+import { useEffect, useState } from "react"
+import { Button } from "@/components/ui/button"
+import MogakoInfoCard from "./components/mogakoCard"
+import cardViewData from "./exampleDatas/cardViewExData.json"
 
 export default function MainPage() {
-  return <>메인페이지</>;
+  const [cards, setCards] = useState<
+    {
+      profileImageUrl: string
+      nickname: string
+      title: string
+      date: string
+      category: string
+      minPeople: number
+      currentPeople: number
+      maxPeople: number
+      hashtags: string[]
+      content: string
+    }[]
+  >([])
+
+  useEffect(() => {
+    // Load the data into state
+    setCards(cardViewData)
+  }, [])
+
+  return (
+    <>
+      {/* Declare a page title */}
+      <title>MOS - Main Page</title>
+      {/* Main Page Part Starts*/}
+      {/* <SearchFieldForMainPage /> */}
+      {/* Category Selector */}
+
+      {/* Category Buttons -> 추후에는 json 에 카테고리를 추가 + for*/}
+      <div className="flex flex-wrap w-full gap-3 max-w-xl items-start justify-between my-5">
+        <Button>모두 표시</Button>
+        <Button>AI</Button>
+        <Button>VR</Button>
+        <Button>백엔드</Button>
+        <Button>프론트엔드</Button>
+        <Button>웹</Button>
+        <Button>뭐든지</Button>
+        <Button>안드로이드</Button>
+        <Button>iOS</Button>
+        <Button>게임</Button>
+        <Button>데이터베이스</Button>
+        <Button>클라우드</Button>
+        <Button>빅데이터</Button>
+        <Button>보안</Button>
+        <Button>블록체인</Button>
+      </div>
+      {/* Card Container */}
+      <div className="flex gap-3 w-full max-w-6xl my-5">
+        <div className="container mx-auto p-4">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            {cards.map((card, index) => (
+              <MogakoInfoCard key={index} {...card} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
