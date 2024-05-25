71ba9cb0c2aec8e1583a3ef4585285b511488d3a
MoonJeWoong <munjin0201@naver.com>
2024-05-25 23:39:27 +0900

    feat: cors 설정 관련 configuration 구현

---
+++ backend/src/main/java/mos/config/WebMvcConfiguration.java
@@ -0,0 +1,17 @@
+package mos.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("*");
+//                .allowCredentials(true);  //이후 인증,인가 기능 추가 시 설정 예정
+    }
+}
