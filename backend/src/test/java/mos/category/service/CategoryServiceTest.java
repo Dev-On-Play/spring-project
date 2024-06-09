86f561edf8736b77b062c6a89b7d2e9784693761
MoonJeWoong <munjin0201@naver.com>
2024-06-09 22:05:36 +0900

    test: 카테고리 전체 조회 서비스 테스트 구현

--- backend/src/test/java/mos/category/service/CategoryServiceTest.java
+++ backend/src/test/java/mos/category/service/CategoryServiceTest.java
@@ -4,7 +4,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import jakarta.persistence.EntityManager;
 import jakarta.transaction.Transactional;
+import mos.category.dto.CategoriesResponse;
 import mos.category.dto.CreateCategoryRequest;
+import mos.category.entity.Category;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
@@ -23,6 +26,21 @@ class CategoryServiceTest {
     @Autowired
     private EntityManager entityManager;
 
+    private Category category1;
+    private Category category2;
+
+    @BeforeEach
+    void setUp() {
+        category1 = Category.createCategory("카테고리 이름1");
+        category2 = Category.createCategory("카테고리 이름2");
+
+        entityManager.persist(category1);
+        entityManager.persist(category2);
+
+        entityManager.flush();
+        entityManager.clear();
+    }
+
     @Test
     void 신규_카테고리를_생성한다() {
         // given
@@ -35,4 +53,12 @@ class CategoryServiceTest {
         assertThat(result).isNotNull();
     }
 
+    @Test
+    void 전체_카테고리를_조회한다() {
+        // given
+        CategoriesResponse response = categoryService.findAll();
+
+        // then
+        assertThat(response.categories()).hasSize(2);
+    }
 }
