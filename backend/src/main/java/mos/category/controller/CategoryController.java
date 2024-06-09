b4bc80e9348f280b631e604828b1b038ef5cb7db
MoonJeWoong <munjin0201@naver.com>
2024-06-09 21:55:41 +0900

    feat: 카테고리 컨트롤러 스웨거 태그 추가

--- backend/src/main/java/mos/category/controller/CategoryController.java
+++ backend/src/main/java/mos/category/controller/CategoryController.java
@@ -1,6 +1,7 @@
 package mos.category.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import mos.category.dto.CategoriesResponse;
 import mos.category.dto.CreateCategoryRequest;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
 
+@Tag(name = "카테고리 관련 API")
 @RestController
 @RequiredArgsConstructor
 public class CategoryController {
