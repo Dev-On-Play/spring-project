e8fee475563e743919b2723870960e54350e4f93
MoonJeWoong <munjin0201@naver.com>
2024-09-13 23:25:13 +0900

    feat: queryDSL queryFactory bean 등록

---
+++ backend/src/main/java/mos/config/BeanConfiguration.java
@@ -0,0 +1,15 @@
+package mos.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BeanConfiguration {
+
+    @Bean
+    JPAQueryFactory jpaQueryFactory(EntityManager em) {
+        return new JPAQueryFactory(em);
+    }
+}
